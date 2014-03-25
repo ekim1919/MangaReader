ef2fe4b62daed59358f181e8c356c9fc8615413c
Wir3dToast <edbeta12@gmail.com>
2014-03-25 01:34:06 -0700

    Added ImgStruct to store Img and pathname

---
+++ MangaReader/Structs/ImgStruct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace MangaReader.Structs {
+
+    internal struct ImgStruct {
+        Image img;
+        String pathname;
+
+        public ImgStruct(Image img, String pathname) {
+            this.img = img;
+            this.pathname = pathname;
+        }
+
+        internal Image getImg {
+            get {
+                return img;
+            }
+        }
+
+        internal string getPath {
+            get {
+                return pathname;
+            }
+        }
+    }
+}
