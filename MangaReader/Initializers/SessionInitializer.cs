adf515c8b02bac3b994dc80c0c7f43dd2e20d3c4
Wir3dToast <edbeta12@gmail.com>
2014-07-05 20:17:52 -0700

    A new static class that is in charge of changing the file manager for the windows manager.

---
+++ MangaReader/Initializers/SessionInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MangaReader.Managers;
+
+namespace MangaReader.Initializers {
+
+    internal static class SessionInitializer {
+
+        public static WindowManager WinMan;
+
+        public static void newFileManager() {
+            FileManager newSession = new FileManager();
+            int chosen_file_index = newSession.Initialize();
+
+            if (chosen_file_index >= 0) {
+                WinMan.ChangeFileManager(newSession, chosen_file_index);
+            }
+        }
+    }
+}
