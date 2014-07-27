befc2e743b42c22d9d86336b35826c2db5854bd7
Wir3dToast <edbeta12@gmail.com>
2014-07-27 01:46:53 -0700

    Changed File Manager to accept FileHandlers to get images from disk or zip file. Redesigned due to the feature concerning reading from a zip file.

--- MangaReader/Managers/FileManager.cs
+++ MangaReader/Managers/FileManager.cs
@@ -7,8 +7,8 @@ using System.Windows.Forms;
 using System.IO;
 using System.Drawing;
 
-using MangaReader.Threads;
 using MangaReader.Structs;
+using MangaReader.FileHandlers;
 /*  
  * 
  * FileManager Class: The class responsible for 
@@ -17,25 +17,23 @@ namespace MangaReader.Managers {
 
     internal class FileManager {
 
-        private readonly List<String> FileNames;
-        private ImagePreload Images;
-
-        public FileManager() {
-            FileNames = new List<String>();
-        }
+        private  FileHandler FileHand;
+        private int FileHandLength;
 
         internal int Initialize() {
             OpenFileDialog fileresult = new OpenFileDialog();
-            fileresult.Filter = "Image Files (*.JPG;*.BMP;*.GIF;*.PNG)|*.JPG;*.BMP;*.GIF;*.PNG";
+            fileresult.Filter = "Image Files (*.JPG;*.BMP;*.GIF;*.PNG)|*.JPG;*.BMP;*.GIF;*.PNG|Zip Files (*.ZIP)|*.ZIP"; 
             fileresult.FilterIndex = 2;
             fileresult.RestoreDirectory = true;
             int FileIndex = -1;
 
             if (fileresult.ShowDialog() == DialogResult.OK) {
                 try {
-                    FileIndex = FindFiles(Path.GetDirectoryName(fileresult.FileName),
-                                          fileresult.FileName);
-                    initializePreload(FileIndex);
+                    FileHand = (Path.GetExtension(fileresult.FileName).Equals(".zip")) ? (FileHandler) new ZipHandler(fileresult.FileName) :
+                                                                                         (FileHandler) new ImgonDiskHandler(fileresult.FileName);
+                    FileIndex = FileHand.initializeImgList();
+                    FileHandLength = FileHand.getImgListLen();
+
                 } catch (Exception a) { //More specific ?
                     MessageBox.Show("Error: " + a.Message);
                 }
@@ -44,44 +42,17 @@ namespace MangaReader.Managers {
             return FileIndex;
         }
 
-        //Add all valid picture files into FileNames List. 
-        private int FindFiles(String SourceDirectory, String SourceFile) {
-            String[] Temp = Directory.GetFiles(SourceDirectory);
-            int ChosenFileIndex = 0; //Defaults to the beginning picture 
-
-            foreach (String file in Temp) {
-                if ((Path.GetExtension(file) == ".jpg") || 
-                    (Path.GetExtension(file) == ".bmp") || 
-                    (Path.GetExtension(file) == ".gif") || 
-                    (Path.GetExtension(file) == ".png")) {
-                    FileNames.Add(file);
-                    
-                    if(file.Equals(SourceFile)) {
-                        ChosenFileIndex = FileNames.IndexOf(file); //Finds the index of the picture the user chose while going through iteration
-                    }
-                    
-                }
-            }
-
-            return ChosenFileIndex;
-        }
-
-        private void initializePreload(int FileIndex) {
-            Images = new ImagePreload(FileNames);
-            Images.LoadImages();
-        }
-
         internal ImgStruct GetNextPos(ref int CurrentPosition) {
-            if (CurrentPosition < FileNames.Count - 1) {
+            if (CurrentPosition < FileHandLength - 1) {
                 CurrentPosition++;
             } else {
                 CurrentPosition = 0; //Start from the beginning  
             }
 
-            bool isLast = (CurrentPosition == 0) ? true : false;
+            bool isLast = (CurrentPosition == 0);
 
-            return new ImgStruct(Images.getImage(CurrentPosition),
-                                 FileNames[CurrentPosition],
+            return new ImgStruct(FileHand.getImage(CurrentPosition),
+                                 FileHand.getImagePath(CurrentPosition),
                                  isLast);
         }
 
@@ -89,25 +60,26 @@ namespace MangaReader.Managers {
             if (CurrentPosition > 0) {
                 CurrentPosition--;
             } else {
-                CurrentPosition = FileNames.Count - 1; //Start from end. 
+                CurrentPosition = FileHandLength - 1; //Start from end. 
             }
-           
-            bool isFront = (CurrentPosition == FileNames.Count - 1) ? true : false;
 
-            return new ImgStruct(Images.getImage(CurrentPosition),
-                                 FileNames[CurrentPosition],
+            bool isFront = (CurrentPosition == FileHandLength - 1);
+
+            return new ImgStruct(FileHand.getImage(CurrentPosition),
+                                 FileHand.getImagePath(CurrentPosition),
                                  isFront);
          }
         
         internal ImgStruct getPicAtPos(ref int CurrentPosition) {
             if (CurrentPosition < 0) {
                 CurrentPosition = 0;
-            } else if (CurrentPosition > FileNames.Count - 1) {
-                CurrentPosition = FileNames.Count - 1;
+            }
+            else if (CurrentPosition > FileHandLength - 1) {
+                CurrentPosition = FileHandLength - 1;
             }
 
-            return new ImgStruct(Images.getImage(CurrentPosition),
-                                 FileNames[CurrentPosition],
+            return new ImgStruct(FileHand.getImage(CurrentPosition),
+                                 FileHand.getImagePath(CurrentPosition),
                                  false);
         }
 
